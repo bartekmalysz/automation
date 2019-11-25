a64451efb482255fea6dc9fae25d76e8f5c370a8
bartekmalysz <bartekmalysz94@gmail.com>
2019-11-25 22:06:14 +0000

    fixed attribite addressing issues

--- tests.py
+++ tests.py
@@ -1,60 +1,40 @@
 import pandas as pd
 import boto3
+pd.options.display.max_columns = 20
+pd.options.display.max_colwidth = 255
 
 client = boto3.client('ec2')
-response = client.describe_instances()
-number_of_instances = len(response['Reservations'])
 resources = boto3.resource('ec2')
 
 
 def get_instances():
+    instances = resources.instances.all()
+    instance_holder = pd.DataFrame()
 
-    instance_summary = pd.DataFrame()
+    for instance in instances:
+        temp = pd.DataFrame(data={
+            'ID': [instance.id],
+            'State': [instance.state['Name']],
+            'KeyName': [instance.key_name],
+            'Type': [instance.instance_type],
+            'AvailabilityZone': [instance.placement['AvailabilityZone']],
+            'Tenancy': [instance.placement['Tenancy']],
+            'VPCID': [instance.vpc_id],
+            'SubnetID': [instance.subnet_id],
+            'PrivateIP': [instance.private_ip_address],
+            'Platform': [instance.platform],
+            'RootDeviceName': [instance.root_device_name],
+            'RootDeviceType': [instance.root_device_type],
+        })
 
-    for instance in range(0, number_of_instances):
-        instance_id = response['Reservations'][instance]['Instances'][0]['InstanceId'] if \
-            len(response['Reservations'][instance]['Instances'][0]['InstanceId']) > 0 else 'none'
-
-        instance_type = response['Reservations'][instance]['Instances'][0]['InstanceType'] if \
-            len(response['Reservations'][instance]['Instances'][0]['InstanceType']) > 0 else 'none'
-
-        instance_state = response['Reservations'][instance]['Instances'][0]['State']['Name'] if \
-            len(response['Reservations'][instance]['Instances'][0]['State']['Name']) > 0 else 'none'
-
-        volume_id = response['Reservations'][instance]['Instances'][0]['BlockDeviceMappings'][0]['Ebs']['VolumeId'] if \
-            len(response['Reservations'][instance]['Instances'][0]['BlockDeviceMappings'][0]['Ebs']['VolumeId']) > 0 \
-            else 'none'
-
-        device_name = response['Reservations'][0]['Instances'][0]['BlockDeviceMappings'][0]['DeviceName'] if \
-            len(response['Reservations'][0]['Instances'][0]['BlockDeviceMappings'][0]['DeviceName']) > 0 else 'none'
-
-        delete_on_termination = response['Reservations'][0]['Instances'][0]['BlockDeviceMappings'][0]['Ebs'][
-            'DeleteOnTermination'] if \
-            len(response['Reservations'][0]['Instances'][0]['BlockDeviceMappings'][0]['Ebs']['DeleteOnTermination']) > \
-            0 else 'none'
-
-        device_status = response['Reservations'][0]['Instances'][0]['BlockDeviceMappings'][0]['Ebs']['Status'] if \
-            len(response['Reservations'][0]['Instances'][0]['BlockDeviceMappings'][0]['Ebs']['Status']) > 0 else 'none'
-
-        instance_holder = pd.DataFrame(data={'instanceId': [instance_id],
-                                             'instanceType': [instance_type],
-                                             'instanceState': [instance_state],
-                                             'volumeId': [volume_id],
-                                             'deviceName': [device_name],
-                                             'delete': [delete_on_termination],
-                                             'deviceStatus': [device_status]
-                                             })
-
-        instance_summary = instance_summary.append(instance_holder, sort=False)
-
-    print('Below instances were found on this AWS account:\n')
-    print(instance_summary)
-    return instance_summary
+        instance_holder = instance_holder.append(temp, ignore_index=True, sort=False)
+    return(instance_holder)
+       # instance_holder.to_csv('./all_instances.csv', sep=',', index=False)
 
 
 def get_running_instances():
     temp = get_instances()
-    running = temp[temp['instanceState'] == 'running']['instanceId'].unique().tolist()
+    running = temp[temp['State'] == 'running']['ID'].unique().tolist()
     return running
 
 
@@ -70,7 +50,7 @@ def stop_running_instances():
 
 def get_stopped_instances():
     temp = get_instances()
-    stopped = temp[temp['instanceState'] == 'stopped']['instanceId'].unique().tolist()
+    stopped = temp[temp['State'] == 'stopped']['ID'].unique().tolist()
     return stopped
 
 
@@ -82,23 +62,6 @@ def start_stopped_instances():
         client.start_instances(InstanceIds=temp)
         print('Started below instances: ')
         print('\n'.join(x for x in temp))
-
-
-def detach_all_drives():
-    temp = get_instances()
-    attached = temp[temp['deviceStatus'] == 'attached']['volumeId'].unique().tolist()
-    if len(attached) < 1:
-        print('All volumes are dettacched')
-    else:
-        ec2 = boto3.resource('ec2')
-        for vol in attached:
-            volume = ec2.Volume(vol)
-
-            volume.detach_from_instance(Device=temp.loc[temp['volumeId'] == vol, 'deviceName'][0],
-                                        Force=True,
-                                        InstanceId=temp.loc[temp['volumeId'] == vol, 'instanceId'][0],
-                                        DryRun=False)
-    print('Detached all drives')
 
 
 def get_detached_drives():
@@ -122,3 +85,54 @@ def delete_detached_drives():
             volume = ec2.Volume(vol)
             volume.delete(DryRun=False)
     print('Deleted all drives')
+
+# subnet_id – The VPC Subnet ID, if running in VPC.
+# vpc_id – The VPC ID, if running in VPC.
+# private_ip_address – The private IP address of the instance.
+# ip_address – The public IP address of the instance.
+# platform – Platform of the instance (e.g. Windows)
+# root_device_name – The name of the root device.
+# root_device_type – The root device type (ebs|instance-store).
+# block_device_mapping – The Block Device Mapping for the instance.
+# state_reason – The reason for the most recent state transition.
+
+
+#
+# Variables:
+# id – The unique ID of the Instance.
+# groups – A list of Group objects representing the security groups associated with the instance.
+# public_dns_name – The public dns name of the instance.
+# private_dns_name – The private dns name of the instance.
+# state – The string representation of the instance’s current state.
+# state_code – An integer representation of the instance’s current state.
+# previous_state – The string representation of the instance’s previous state.
+# previous_state_code – An integer representation of the instance’s current state.
+# key_name – The name of the SSH key associated with the instance.
+# instance_type – The type of instance (e.g. m1.small).
+# launch_time – The time the instance was launched.
+# image_id – The ID of the AMI used to launch this instance.
+# placement – The availability zone in which the instance is running.
+# placement_group – The name of the placement group the instance is in (for cluster compute instances).
+# placement_tenancy – The tenancy of the instance, if the instance is running within a VPC. An instance with a tenancy of dedicated runs on a single-tenant hardware.
+# kernel – The kernel associated with the instance.
+# ramdisk – The ramdisk associated with the instance.
+# architecture – The architecture of the image (i386|x86_64).
+# hypervisor – The hypervisor used.
+# virtualization_type – The type of virtualization used.
+# product_codes – A list of product codes associated with this instance.
+# ami_launch_index – This instances position within it’s launch group.
+# monitored – A boolean indicating whether monitoring is enabled or not.
+# monitoring_state – A string value that contains the actual value of the monitoring element returned by EC2.
+# spot_instance_request_id – The ID of the spot instance request if this is a spot instance.
+# subnet_id – The VPC Subnet ID, if running in VPC.
+# vpc_id – The VPC ID, if running in VPC.
+# private_ip_address – The private IP address of the instance.
+# ip_address – The public IP address of the instance.
+# platform – Platform of the instance (e.g. Windows)
+# root_device_name – The name of the root device.
+# root_device_type – The root device type (ebs|instance-store).
+# block_device_mapping – The Block Device Mapping for the instance.
+# state_reason – The reason for the most recent state transition.
+# interfaces – List of Elastic Network Interfaces associated with this instance.
+# ebs_optimized – Whether instance is using optimized EBS volumes or not.
+# instance_profile – A Python dict containing the instance profile id and arn associated with this instance.
